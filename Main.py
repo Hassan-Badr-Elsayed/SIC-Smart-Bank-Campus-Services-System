144b671fb78e0f5cae0240a96bc4a31927d78300
Hassan_Badr_Elsayed <hassanbadr33333@gmail.com>
2026-08-23 19:11:44 +0300

    edit_user

--- Main.py
+++ Main.py
@@ -213,137 +213,137 @@ To close the system, enter exit
                 print("[5] Branch/ATM status")
                 print("[6] Update personal info")
                 print("[7] Exit")
-                while not user_input:
+                
 
-                    user_input = input("please enter what do you want to do: ")
+                user_input = input("please enter what do you want to do: ")
                     
 
-                    if (user_input == Deposit):
-                        deposit_value,currency = input("please enter amount and currency: ").split()
-                        if(deposit_value.isdigit()):
-                            deposit_value = int(deposit_value)
+                if (user_input == Deposit):
+                    deposit_value,currency = input("please enter amount and currency: ").split()
+                    if(deposit_value.isdigit()):
+                        deposit_value = int(deposit_value)
 
-                            if (deposit_value <= 0 and (currency != "EGP" and currency != "SAR" and currency != "USD")):
-                                print("Invalid value and currency")
-                                continue
-                            elif (deposit_value <= 0):
-                                print("Invalid value")
-                                continue
-                            elif (currency != "EGP" and currency != "SAR" and currency != "USD"):
-                                print("Invalid value")
-                                continue
-
-                            if (currency == "SAR" or currency == "USD" or currency == "EGP"):
-                                if(currency == "SAR"):
-                                    cureent_user["wallet"][currency] = "SAR"
-                                    amount_in_egp = deposit_value *9
-                                    transaction_msg = f"Deposit: {deposit_value} {currency} (+{amount_in_egp} EGP)"
-                                    cureent_user["hestory"].append(transaction_msg)
-                                    cureent_user["wallet"]["balance"] = amount_in_egp
-
-
-                                elif (currency == "USD"):
-                                    cureent_user["wallet"][currency] = "USD"
-                                    amount_in_egp = deposit_value *30
-                                    transaction_msg = f"Deposit: {deposit_value} {currency} (+{amount_in_egp} EGP)"
-                                    cureent_user["hestory"].append(transaction_msg)
-                                    cureent_user["wallet"]["balance"] = amount_in_egp
-
-                                else:
-                                    cureent_user["wallet"][currency] = "EGP"
-                                    amount_in_egp = deposit_value
-                                    transaction_msg = f"Deposit: {deposit_value} {currency}"
-                                    cureent_user["hestory"].append(transaction_msg)
-                                    cureent_user["wallet"]["balance"] = amount_in_egp
-                        else:
-                            print("Invalid input")
-                            print("the process isn't success")
+                        if (deposit_value <= 0 and (currency != "EGP" and currency != "SAR" and currency != "USD")):
+                            print("Invalid value and currency")
                             continue
-            ########################################################################################################################
-                    elif (user_input == Withdraw):    
-                        withdraw_value = input("please enter the value: ")
-                        if(withdraw_value.isdigit()):
-                            withdraw_value = int(withdraw_value)
-                            if(withdraw_value <= 0):
-                                print("Invalid input")
-                                print("the process isn't sucsess please try again")
+                        elif (deposit_value <= 0):
+                            print("Invalid value")
+                            continue
+                        elif (currency != "EGP" and currency != "SAR" and currency != "USD"):
+                            print("Invalid value")
+                            continue
+
+                        if (currency == "SAR" or currency == "USD" or currency == "EGP"):
+                            if(currency == "SAR"):
+                                cureent_user["wallet"][currency] = "SAR"
+                                amount_in_egp = deposit_value *9
+                                transaction_msg = f"Deposit: {deposit_value} {currency} (+{amount_in_egp} EGP)"
+                                cureent_user["hestory"].append(transaction_msg)
+                                cureent_user["wallet"]["balance"] = amount_in_egp
+
+
+                            elif (currency == "USD"):
+                                cureent_user["wallet"][currency] = "USD"
+                                amount_in_egp = deposit_value *30
+                                transaction_msg = f"Deposit: {deposit_value} {currency} (+{amount_in_egp} EGP)"
+                                cureent_user["hestory"].append(transaction_msg)
+                                cureent_user["wallet"]["balance"] = amount_in_egp
+
+                            else:
+                                cureent_user["wallet"][currency] = "EGP"
+                                amount_in_egp = deposit_value
+                                transaction_msg = f"Deposit: {deposit_value} {currency}"
+                                cureent_user["hestory"].append(transaction_msg)
+                                cureent_user["wallet"]["balance"] = amount_in_egp
+                    else:
+                        print("Invalid input")
+                        print("the process isn't success")
+                        continue
+        ########################################################################################################################
+                elif (user_input == Withdraw):    
+                    withdraw_value = input("please enter the value: ")
+                    if(withdraw_value.isdigit()):
+                        withdraw_value = int(withdraw_value)
+                        if(withdraw_value <= 0):
+                            print("Invalid input")
+                            print("the process isn't sucsess please try again")
+                            continue
+                        else:
+                            if(withdraw_value > cureent_user["wallet"]["balance"]):
+                                print("you don't have enogh money")
+                                print("the process isn't sucsess")
                                 continue
                             else:
-                                if(withdraw_value > cureent_user["wallet"]["balance"]):
-                                    print("you don't have enogh money")
-                                    print("the process isn't sucsess")
-                                    continue
-                                else:
-                                    cureent_user["wallet"]["balance"] -= withdraw_value
+                                cureent_user["wallet"]["balance"] -= withdraw_value
 
-                                    transaction_msg = f"Withdraw: {withdraw_value}"
-                                    cureent_user["hestory"].append(transaction_msg)
-                                    print("process success")
-                        else:
-                            print("Invalid value")
-                            print("the process isn't sucsess please try again")
-
-            ############################################################################################################################
-                    elif (user_input == Transfer):
-                        id_target_user = int(input("please enter the id of the target user: "))
-                        for u in users:
-                            if u["id"] == id_target_user:
-                                target_user = u
-                        if (target_user == cureent_user):
-                            print("you can't transfer to your self")
-                            continue
-                        elif(target_user == "no one"):
-                            print("Invalid Id number")
-                            continue
-
-
-                        transfer_value = input("Enter The Transfer value: ")
-
-                        if (transfer_value <= 0):
-                            print("Invalid input")
-                            print("the Transfer isn't sucsess please try again")
-                            continue
-                        else:
-                            if(transfer_value > cureent_user["wallet"]["balance"]):
-                                print("you don't have enogh money please deposit money and try again")
-                                print("the Transfer isn't sucsess ")
-                            elif(transfer_value < cureent_user["wallet"]["balance"]):
-                                cureent_user["wallet"]["balance"] -= transfer_value
-                                transaction_msg = f"transfer to {target_user["profile"]["name"]}: {transfer_value}"
+                                transaction_msg = f"Withdraw: {withdraw_value}"
                                 cureent_user["hestory"].append(transaction_msg)
-
-                                target_user["wallet"]["balance"] += transfer_value
-                                transaction_msg = f"transfer from {cureent_user["profile"]["name"]}: {transfer_value}"
-                                cureent_user["hestory"].append(transaction_msg)
-                                print("Transfer success")
-
-            ###########################################################################################################################
-                    elif (user_input == Transaction_history):
-                        if (cureent_user["hestory"] == []):
-                            print("you don't have Transaction_history")
-                            continue
-                        else:
-                            for i in cureent_user["hestory"]:
-                                print(i)
-            ##################################################################################################################################
-                    elif (user_input == Reports):
-                        print()
-                    elif (user_input == Branch_ATM_status):
-                        print()
-                    elif (user_input == Update_personal_info):
-                        print()
-                    elif (user_input == Exit):
-                        print("End session")
-                        break
+                                print("process success")
                     else:
-                        print("Invalid Input")
-                        print("enter number from the menu")
+                        print("Invalid value")
+                        print("the process isn't sucsess please try again")
+
+        ############################################################################################################################
+                elif (user_input == Transfer):
+                    id_target_user = int(input("please enter the id of the target user: "))
+                    for u in users:
+                        if u["id"] == id_target_user:
+                            target_user = u
+                    if (target_user == cureent_user):
+                        print("you can't transfer to your self")
+                        continue
+                    elif(target_user == "no one"):
+                        print("Invalid Id number")
+                        continue
+
+
+                    transfer_value = input("Enter The Transfer value: ")
+
+                    if (transfer_value <= 0):
+                        print("Invalid input")
+                        print("the Transfer isn't sucsess please try again")
+                        continue
+                    else:
+                        if(transfer_value > cureent_user["wallet"]["balance"]):
+                            print("you don't have enogh money please deposit money and try again")
+                            print("the Transfer isn't sucsess ")
+                        elif(transfer_value < cureent_user["wallet"]["balance"]):
+                            cureent_user["wallet"]["balance"] -= transfer_value
+                            transaction_msg = f"transfer to {target_user["profile"]["name"]}: {transfer_value}"
+                            cureent_user["hestory"].append(transaction_msg)
+
+                            target_user["wallet"]["balance"] += transfer_value
+                            transaction_msg = f"transfer from {cureent_user["profile"]["name"]}: {transfer_value}"
+                            cureent_user["hestory"].append(transaction_msg)
+                            print("Transfer success")
+
+        ###########################################################################################################################
+                elif (user_input == Transaction_history):
+                    if (cureent_user["hestory"] == []):
+                        print("you don't have Transaction_history")
+                        continue
+                    else:
+                        for i in cureent_user["hestory"]:
+                            print(i)
+        ##################################################################################################################################
+                elif (user_input == Reports):
+                    print()
+                elif (user_input == Branch_ATM_status):
+                    print()
+                elif (user_input == Update_personal_info):
+                    print()
+                elif (user_input == Exit):
+                    print("End session")
+                    break
+                else:
+                    print("Invalid Input")
+                    print("enter number from the menu")
 
 
 
 
 
-                        
+                    
             # You can place your user menu / update personal info here
             
     elif option == "exit":
